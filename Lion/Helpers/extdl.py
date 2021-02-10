8b30f07dcc979edd50842129ac7cc990a985e844
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-02-10 10:52:32 +0530

    Delete extdl.py

--- Lion/Helpers/extdl.py
+++
@@ -1,7 +0,0 @@
-from subprocess import PIPE, Popen
-
-
-def install_pip(pipfile):
-    process = Popen(["pip", "install", f"{pipfile}"], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    return stdout
